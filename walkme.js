240a9180edbec5b53e9663331dc208aeb330e5d1
subodhsahu <subodhsahu@users.noreply.github.com>
2023-03-17 17:58:42 +0530

    Create walkme.js

---
+++ walkme.js
@@ -0,0 +1,9 @@
+(function() {
+	var walkme = document.createElement('script'); 
+	walkme.type = 'text/javascript'; 
+	walkme.async = true; 
+	walkme.src = 'https://cdn.walkme.com/users/4f42ff5d9a6040f9b3b00aefaaee64e8/test/walkme_4f42ff5d9a6040f9b3b00aefaaee64e8_https.js'; 
+	var s = document.getElementsByTagName('script')[0]; 
+	s.parentNode.insertBefore(walkme, s); 
+	window._walkmeConfig = {smartLoad:true}; 
+})();
