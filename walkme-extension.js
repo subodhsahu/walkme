529bd829caeec91c34bd5ea731adc9c78673c608
subodhsahu <subodhsahu@users.noreply.github.com>
2023-03-21 01:53:13 +0530

    Update walkme-extension.js

--- walkme-extension.js
+++ walkme-extension.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     tableau.extensions.initializeAsync().then(function() {
 	var dashboard = tableau.extensions.dashboardContent.dashboard;
-	console.log('Initialization Successful');
+	console.log('Walkme Extension Initialization Successful');
 	/*
 	tableau.extensions.dashboardContent.dashboard.objects.forEach(function(object){
         	console.log(object.name + ":" + object.type + ":" + object.id + ":" + object.isVisible);
